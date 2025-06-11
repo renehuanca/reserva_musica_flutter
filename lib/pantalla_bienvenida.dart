becce4d848d1c313487a600d9ac2995d1f7751a3
DrimerN <148470800+DrimerN@users.noreply.github.com>
2025-06-10 17:30:45 -0700

    Update pantalla_bienvenida.dart

--- lib/pantalla_bienvenida.dart
+++ lib/pantalla_bienvenida.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/material.dart';
+import 'pantalla_inicio_sesion.dart';
+
+class PantallaBienvenida extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.music_note, size: 100, color: Colors.brown),
+              SizedBox(height: 20),
+              Text(
+                'ReservaMúsica',
+                style: TextStyle(
+                  fontSize: 38,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PantallaInicioSesion(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Empezar',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.brown,
+                  padding: EdgeInsets.symmetric(horizontal: 55, vertical: 25),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
